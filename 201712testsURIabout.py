126de9419503f42138b167c0075457070bf77bf3
Lully <lully1804@yahoo.fr>
2017-12-20 10:30:20 +0100

    Tests URI

--- 201712testsURIabout.py
+++ 201712testsURIabout.py
@@ -6,28 +6,40 @@ Created on Thu Dec 14 14:01:25 2017
 """
 
 from lxml import etree
+from lxml.html import parse
 from urllib import request
 from collections import defaultdict
 
-ns = {"bnf-onto":"http://data.bnf.fr/ontology/bnf-onto/", "bnfroles":"http://data.bnf.fr/vocabulary/roles/", "dcterms":"http://purl.org/dc/terms/", "foaf":"http://xmlns.com/foaf/0.1/", "marcrel":"http://id.loc.gov/vocabulary/relators/", "owl":"http://www.w3.org/2002/07/owl#", "rdagroup1elements":"http://rdvocab.info/Elements/", "rdarelationships":"http://rdvocab.info/RDARelationshipsWEMI/", "rdf":"http://www.w3.org/1999/02/22-rdf-syntax-ns#", "rdfs":"http://www.w3.org/2000/01/rdf-schema#"}
+ns = {"bnf-onto":"http://data.bnf.fr/ontology/bnf-onto/", 
+      "bnfroles":"http://data.bnf.fr/vocabulary/roles/", 
+      "dcterms":"http://purl.org/dc/terms/", 
+      "foaf":"http://xmlns.com/foaf/0.1/", 
+      "marcrel":"http://id.loc.gov/vocabulary/relators/", 
+      "owl":"http://www.w3.org/2002/07/owl#", 
+      "rdagroup1elements":"http://rdvocab.info/Elements/", 
+      "rdarelationships":"http://rdvocab.info/RDARelationshipsWEMI/", 
+      "rdf":"http://www.w3.org/1999/02/22-rdf-syntax-ns#", 
+      "rdfs":"http://www.w3.org/2000/01/rdf-schema#"}
 
 urlroot = input("URL racine : ")
 
 if (urlroot == ""):
-    urlroot = "https://extranet.logilab.fr/demo/BNF/demo/"
+    urlroot = "http://pfvdata.bnf.fr/"
 
 #URI test page Auteur
 URIaut = "11896834/christine_de_pisan/"
-ARKaut = "ark:/12148/cb11896834h"
+pageAUT = parse(urlroot + URIaut)
+ARKaut = pageAUT.find("//meta[@name='DC.identifier']").get("content")
 #URI manifestation pour tests Expression, Oeuvre, Concept
-ARKmanif = "ark:/12148/cb45281463d"
+ARKmanif = "ark:/12148/cb380880420"
 #URI manifestation pour test relation sujet
-ARKmanifSujet = "ark:/12148/cb357071723"
+ARKmanifSujet = "ark:/12148/cb358430469"
 
 resultats = defaultdict(bool)
 
     
 def func_test_generique(page,path):
+    print(path)
     test = False
     if (page.find(path, namespaces=ns) is True):
         test = True
@@ -41,20 +53,31 @@ def func_test_generique(page,path):
 #     
 #==============================================================================
 
-def checkRDFAuthor(URLpage,ARK):
-    URIautRDFXML = URLpage + "rdf.xml"
+def checkRDFAuthor(URIauteur,ARK):
+    URIautRDFXML = urlroot + URIauteur + "rdf.xml"
     page = etree.parse(request.urlopen(URIautRDFXML))
 #test 1.a
-    path = "//rdf:Description[@rdf:about='" + urlroot + ARK + "']/rdf:type[contains(@rdf:resource, 'http://xmlns.com/foaf/0.1')]"
-    resultats["Auteur_testType"] = func_test_generique(page,path)
+    path = "//rdf:Description[@rdf:about='" + urlroot + ARK + "#about']/rdf:type"
+    resultats["Auteur_testType"] = func_test_attribut(page,path, "rdf:resource", "http://xmlns.com/foaf/0.1","contains")
 #test 1.b  
-    path = "//rdf:Description[@rdf:about='" + urlroot + ARK + "']/foaf:focus[@rdf:resource='" + urlroot + ARK + "#about" + "')]"      
+    path = "//rdf:Description[@rdf:about='" + urlroot + ARK + "']/foaf:focus[@rdf:resource='" + urlroot + ARK + "#about" + "']"      
     resultats["Auteur_testfocus"] = func_test_generique(page,path)
 #test 1.c
-    path = "//rdf:Description[@rdf:about='" + urlroot + ARK + "#about']/owl:sameAs[contains(@rdf:resource,'" + urlroot + ARK + "#foaf')]"
+    path = "//rdf:Description[@rdf:about='" + urlroot + ARK + "#about']/owl:sameAs[@rdf:resource='" + urlroot + ARK + "#foaf:Person']"
     resultats["Auteur_testsameAs"] = func_test_generique(page,path)
 
 
+def func_test_attribut(page,path,attr_name,string,test):
+    test = False
+    count = 0
+    if (test == "contains"):
+        for node in page.xpath(path, namespaces=ns):
+            attr_value = node.get(attr_name)
+            if (attr_value.find(string) > -1):
+                count += 1
+    if (count > 0):
+        test = True
+    return test
 #==============================================================================
 # 2. Vérifier qu'une sérialisation RDF d'un ARK de manifestation contient :
 # 	a. ARKmanif#about a frbr-rda:Manifestation
@@ -83,11 +106,11 @@ def checkRDFManif(ARKmanif):
     path = "//rdf:Description[@rdf:about='" + urlroot + ARKmanif + "']/foaf:focus[@rdf:resource='" + ARKmanif + "#about']"
     resultats["Manif_foaf_focus"] = func_test_generique(page,path)        
 #test 2.e
-    path = "//rdf:Description[@rdf:about='" + urlroot + ARKmanif + "']/rdf:type[@rdf:resource='http://xmlns.com/foaf/0.1/Document']"
+    path = "//rdf:Description[@rdf:about='" + urlroot + ARKmanif + "']/rdf:type[@rdf:resource='http://www.w3.org/2004/02/skos/core#Concept']"
     resultats["Manif_foaf_document"] = func_test_generique(page,path)        
 #test 2.f
-    path = "//rdf:Description[@rdf:about='" + urlroot + ARKmanif + "']/rdarelationships:workManifested[contains(@rdf:resource,'#about')]"
-    resultats["Manif_lien_oeuvre"] = func_test_generique(page,path)        
+    path = "//rdf:Description[@rdf:about='" + urlroot + ARKmanif + "#about']/rdarelationships:workManifested[contains(@rdf:resource,'#about')]"
+    resultats["Manif_lien_oeuvre"] = func_test_attribut(page,path,"rdf:resource","#about","contains")        
 #==============================================================================
 # 3. Vérifier la relation manifestation - sujet
 # 	a. ARKmanif#about dcterms:subject ARKsujet
@@ -95,8 +118,8 @@ def checkRDFManif(ARKmanif):
 def checkRDFManifSujet(ARKmanif):
     URImanifRDFXML = urlroot + ARKmanif
     page = etree.parse(request.urlopen(URImanifRDFXML))
-    path = "//rdf:Description[@rdf:about='" + urlroot + ARKmanif + "#about']/dcterms:subject[contains(@rdf:resource,'ark')]"
-    resultats["Manif_Sujet"] = func_test_generique(page,path)        
+    path = "//rdf:Description[@rdf:about='" + urlroot + ARKmanif + "#about']/dcterms:subject"
+    resultats["Manif_Sujet"] = func_test_attribut(page,path,"rdf:resource","ark","contains")        
 
 
 def checkAll():
